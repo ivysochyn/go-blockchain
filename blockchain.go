8c568857ba10cf4de1eb47f6768d4dc7b17f0fe0
Illia Vysochyn <ivysochyn@internships.antmicro.com>
2022-08-15 21:53:33 +0200

    [#1] blockchain.go: Add calculation for total amount of currency for every user

--- blockchain.go
+++ blockchain.go
@@ -92,6 +92,24 @@ func (bc *Blockchain) Mining() bool {
     log.Println("action=mining, status=success")
     return true
 }
+
+func (bc *Blockchain) CalculateTotalAmount(blockchainAddress string) float32 {
+    var totalAmount float32 = 0.0
+    for _, b := range bc.chain {
+        for _, t := range b.transactions {
+            value := t.value
+            if blockchainAddress == t.recipientBlockchainAddress {
+                totalAmount += value
+            }
+
+            if blockchainAddress == t.senderBlockchainAddress {
+                totalAmount -= value
+            }
+        }
+    }
+    return totalAmount
+}
+
 /* --------------------------------------------------- */
 
 type Block struct {
@@ -189,4 +207,8 @@ func main() {
     blockChain.AddTransaction("X", "Y", float32(3))
     blockChain.Mining()
     blockChain.Print()
+
+    fmt.Printf("C: %.1f\n", blockChain.CalculateTotalAmount("C"))
+    fmt.Printf("D: %.1f\n", blockChain.CalculateTotalAmount("D"))
+    fmt.Printf("ME: %.1f\n", blockChain.CalculateTotalAmount("my_blockchain_address"))
 }
